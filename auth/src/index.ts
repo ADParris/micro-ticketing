7d6a2d85db3f1c5213e373c433923f54a82d138f
Andrew Parris <andrew@adparris.com>
2020-07-12 05:35:36 -0700

    feature: Day 2 - Auth & Login functionality completed.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,50 @@
-import { json } from 'body-parser';
 import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
 
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(currentUserRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 
-app.listen(PORT, () =>
-  console.log(`Auth server is listening on port ${PORT}...`)
-);
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined.');
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Auth server is connected to MongoDB.');
+  } catch (error) {
+    console.error(error.message);
+  }
+  app.listen(PORT, () =>
+    console.log(`Auth server is listening on port ${PORT}...`)
+  );
+};
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -12,16 +18,29 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters in length.'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
-    console.log('Creating user...');
-    res.send({});
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError('Email already in use.');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+    req.session = { jwt: userJwt };
+
+    res.status(201).send(user);
   }
 );
 
